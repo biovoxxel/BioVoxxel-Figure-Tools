968a2c9adc3b2ede6fc8cc7adc8becdf28aba3a4
Jan Brocher <biovoxxel@users.noreply.github.com>
2024-11-25 15:39:48 +0100

    push to version 4.0.0

--- biovoxxel-figure-tools/src/main/java/svg/gui/SVGExporter.java
+++ biovoxxel-figure-tools/src/main/java/svg/gui/SVGExporter.java
@@ -37,6 +37,9 @@ public class SVGExporter extends DynamicCommand {
 	@Parameter(label = "Keep multichannel composite", required = true, description = "keeps channels accessible in Inkscape but increases file size")
 	Boolean keepComposite = false;
 	
+	@Parameter(label = "Make merge interactive", required = true, description = "")
+	Boolean makeInteractive = false;
+	
 	@Parameter(label = "Export channels", choices = {"None", "Color", "Grayscale", "Color (no overlays)", "Grayscale (no overlays)"})
 	String exportChannelsSeparately = "None";
 	
@@ -51,7 +54,7 @@ public class SVGExporter extends DynamicCommand {
 	
 	public void run() {
 			
-		SVG_Object_Factory.saveImageAndOverlaysAsSVG(imp, createSVGFile(), interpolationRange, keepComposite, true, lockSensitiveROIs);
+		SVG_Object_Factory.saveImageAndOverlaysAsSVG(imp, createSVGFile(), interpolationRange, keepComposite, makeInteractive, true, lockSensitiveROIs);
 		
 		if (!exportChannelsSeparately.equalsIgnoreCase("none") && imp.isComposite()) {
 			exportIndividualChannels();			
@@ -78,7 +81,7 @@ public class SVGExporter extends DynamicCommand {
 					currentChannel.setOverlay(imp.getOverlay());
 				}
 				System.out.println("fileName = " + fileName);
-				SVG_Object_Factory.saveImageAndOverlaysAsSVG(currentChannel, createSVGFile(), interpolationRange, false, true, lockSensitiveROIs);
+				SVG_Object_Factory.saveImageAndOverlaysAsSVG(currentChannel, createSVGFile(), interpolationRange, false, false, true, lockSensitiveROIs);
 			}
 		}
 	}	
